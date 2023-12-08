8cfd53fca82df8eeb1b700677435981d40fdc14f
MandapatGilmar <102348195+MandapatGilmar@users.noreply.github.com>
2023-12-08 12:18:56 +0800

    Add files via upload

--- purchase_order_view.php
+++ purchase_order_view.php
@@ -9,21 +9,36 @@ if (isset($_POST['id'])) {
     $orderDetails = mysqli_fetch_assoc($orderQuery);
 
     if ($orderDetails) {
+        // Fetch the purchase code
+        $purchaseCode = $orderDetails['purchasecode']; // Replace 'purchasecode' with the actual column name in your database
+        $supplierName = $orderDetails['supplier']; // Replace 'suppliername' with the actual column name in your database
         // Fetch related items for this purchase order
         $itemsQuery = mysqli_query($conn, "SELECT * FROM purchase_order_items WHERE purchase_order_id = '$purchaseId'");
 
-        // Start the table
-        echo "<table class='table table-striped table-bordered'>";
-        // Table headers
-        echo "<thead><tr class='text-light bg-navy'>
-            <th class='text-center py-1 px-2'>#</th>
-            <th class='text-center py-1 px-2'>Qty</th>
-            <th class='text-center py-1 px-2'>Unit</th>
-            <th class='text-center py-1 px-2'>Item</th>
-            <th class='text-center py-1 px-2'>Attributes</th>
-            <th class='text-center py-1 px-2'>Price</th>
-            <th class='text-center py-1 px-2'>Total</th>
-        </tr></thead>";
+        // Header of the receipt
+        echo "<div style='text-align: center; margin-bottom: 20px;'>
+                <h3 style='color: #02964C;'>Purchase Order</h3>
+                <p style='margin: 0;'><strong>Purchase Order Code:</strong> {$purchaseCode}</p> <!-- Use the fetched purchase code here -->
+                <p style='margin: 0;'><strong>Supplier:</strong> {$supplierName}</p> 
+                <p style='margin: 0;'><strong>Date:</strong>" . date("Y-m-d H:i:s") . "</p>
+              </div>";
+
+        // Start the table with a simpler design
+        echo "<table style='width: 100%; border-collapse: collapse;'>";
+
+        // Table headers with color
+        echo "<thead style='background-color: #02964C; color: white;'>
+                <tr>
+                    <th class='text-center py-1 px-2'>#</th>
+                    <th class='text-center py-1 px-2'>Qty</th>
+                    <th class='text-center py-1 px-2'>Unit</th>
+                    <th class='text-center py-1 px-2'>Item</th>
+                    <th class='text-center py-1 px-2'>Attributes</th>
+                    <th class='text-center py-1 px-2'>Price</th>
+                    <th class='text-center py-1 px-2'>Total</th>
+                </tr>
+              </thead>";
+
         echo "<tbody>";
 
         // Display each item in a table row
@@ -31,26 +46,26 @@ if (isset($_POST['id'])) {
         while ($itemRow = mysqli_fetch_assoc($itemsQuery)) {
             $totalPrice = $itemRow['productprice'] * $itemRow['productquantity'];
             echo "<tr>
-                <td class='text-center'>{$count}</td>
-                <td class='text-center'>{$itemRow['productquantity']}</td>
-                <td class='text-center'>{$itemRow['productunit']}</td>
-                <td class='text-center'>{$itemRow['productname']}</td>
-                <td class='text-center'>{$itemRow['productattributes']}</td>
-                <td class='text-center'>₱{$itemRow['productprice']}</td>
-                <td class='text-center'>₱{$totalPrice}</td>
-            </tr>";
+                    <td class='text-center'>{$count}</td>
+                    <td class='text-center'>{$itemRow['productquantity']}</td>
+                    <td class='text-center'>{$itemRow['productunit']}</td>
+                    <td class='text-center'>{$itemRow['productname']}</td>
+                    <td class='text-center'>{$itemRow['productattributes']}</td>
+                    <td class='text-center'>₱{$itemRow['productprice']}</td>
+                    <td class='text-center'>₱{$totalPrice}</td>
+                  </tr>";
             $count++;
         }
 
         echo "</tbody></table>";
 
         // Display purchase order totals
-        echo "<div class='totals'>
-            <p><strong>Subtotal:</strong> ₱{$orderDetails['poSubtotal']}</p>
-            <p><strong>Discount:</strong> ₱{$orderDetails['poDiscountTotal']}</p>
-            <p><strong>Tax:</strong> ₱{$orderDetails['poTaxTotal']}</p>
-            <p><strong>Grand Total:</strong> ₱{$orderDetails['poGrandtotal']}</p>
-        </div>";
+        echo "<div style='text-align: right; margin-top: 10px;'>
+                <p><strong>Subtotal:</strong> ₱{$orderDetails['poSubtotal']}</p>
+                <p><strong>Discount:</strong> ₱{$orderDetails['poDiscountTotal']}</p>
+                <p><strong>Tax:</strong> ₱{$orderDetails['poTaxTotal']}</p>
+                <p><strong>Grand Total:</strong> <span style='color: #02964C;'>₱{$orderDetails['poGrandtotal']}</span></p>
+              </div>";
     } else {
         echo "<p class='text-danger'><b>Purchase order details not found.</b></p>";
     }
